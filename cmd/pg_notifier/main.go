26aef2a2968b869ed0fac463e0d7f78d551d4505
wilbyang <yang.wilby@gmail.com>
2025-04-25 00:17:46 +0200

    pg notifier

--- cmd/pg_notifier/main.go
+++ cmd/pg_notifier/main.go
@@ -7,6 +7,32 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+/*
+ *
+create trigger doc_notify
+    after insert or update on documents
+    for each row
+    execute function notify_document_change();
+create or replace function notify_document_change()
+    returns trigger as $$
+declare
+    payload text;
+    channel text := 'document_changes';
+begin
+    payload := json_build_object(
+        'operation', TG_OP,
+        'schema', TG_TABLE_NAME,
+        'table', TG_TABLE_NAME,
+        'record', to_jsonb(NEW),
+        'old_record', to_jsonb(OLD)
+    )::text;
+
+    perform pg_notify(channel, payload);
+    return NEW;
+end;
+$$ language plpgsql;
+*/
+// insert into documents(title, content, doc_size, created_at, updated_at, status, author_id, file_path) select title, content, doc_size, created_at, updated_at, status, author_id, file_path from documents order by id limit 1;
 func main() {
 	ctx := context.TODO()
 	//pgx v5 connection
@@ -22,7 +48,7 @@ func main() {
 	}
 	defer conn.Release()
 
-	_, err = conn.Exec(context.Background(), "LISTEN test_topic")
+	_, err = conn.Exec(context.Background(), "LISTEN document_changes")
 	if err != nil {
 		log.Fatalf("Unable to listen to task_queue: %v\n", err)
 	}
